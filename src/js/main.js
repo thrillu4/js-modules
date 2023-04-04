8f2ff44da7807d1591ad1983765c64787d16f003
Denis <tonopik@gmail.com>
2023-04-04 21:23:32 +0300

    add slides

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
 import modals from "./modules/modals";
+import slides from "./modules/slides";
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
   modals();
+  slides(
+    ".feedback-slider-item",
+    "horizontal",
+    ".main-prev-btn",
+    ".main-next-btn"
+  );
+
+  slides(".main-slider-item", "vertical");
 });
