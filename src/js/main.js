1dc325677a99b8f245948ddfd11e3025c17643d5
Denis <tonopik@gmail.com>
2023-04-06 17:50:16 +0300

    form realization

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import modals from "./modules/modals";
 import slides from "./modules/slides";
+import forms from "./modules/forms";
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
@@ -10,6 +11,6 @@ window.addEventListener("DOMContentLoaded", () => {
     ".main-prev-btn",
     ".main-next-btn"
   );
-
   slides(".main-slider-item", "vertical");
+  forms();
 });
